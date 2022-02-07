043bd32c95a92416559db88cc90a9cda4ab53da2
alfand-alef <alfand.alef@gmail.com>
2022-02-07 17:12:41 +0300

    Эксперимент с web sql

--- public_html/testing/js/testing-main.js
+++ public_html/testing/js/testing-main.js
@@ -6,7 +6,7 @@
 
 function getParent(jsonObj, parentID) {
     if (0 == parentID) {
-        return '–';
+        return ' ';
     }
     var M = jsonObj.length;
     for (ii = 0; M > ii; ii++) {
@@ -14,6 +14,23 @@ function getParent(jsonObj, parentID) {
             return jsonObj[ii].name;
         }
     }
+}
+
+function getDataFromMoodle(jsonData) {
+    //var jsn = jsonData;
+    if (typeof jsonData.errorcode !== undefined) {
+        var N = jsonData.length;
+        for (i = 0; N > i; i++) {
+            console.log((0 == jsonData[i].coursecount) ? ('–') : (jsonData[i].coursecount));
+            $('#tbl_data').append('<tr>'
+                    + '<td>' + jsonData[i].id + '</td>'
+                    + '<td><a target="_blank" href="https://study.edu.tele-med.ai/course/index.php?categoryid=' + jsonData[i].id + '">' + jsonData[i].name + '</a>' + '</td>'
+                    + '<td>' + ((0 == jsonData[i].coursecount) ? (' ') : (jsonData[i].coursecount)) + '</td>'
+                    + '<td>' + getParent(jsonData, jsonData[i].parent) + '</td>'
+                    + '</tr>');
+        }
+    }
+    return;
 }
 
 $(document).ready(function () {
@@ -26,32 +43,16 @@ $(document).ready(function () {
         wsfunction: functionname,
         moodlewsrestformat: 'json',
         //id: 73 //Retrieve results based on course Id 2
-    }
-
-    var response = $.ajax(
-            {type: 'GET',
-                data: data,
-                dataType: "json", // тип загружаемых данных
-                url: serverurl,
-                success: function (data, textStatus) {
-                    var jsn = data;
-                    console.log(jsn);
-                    var jsn = data;
-                    if (typeof jsn.errorcode !== undefined) {
-                        //$('#data_out').html(JSON.stringify(jsn[1]));
-                        //$('#data_out').append('<table id="tbl_data" class="table table-striped"><t></table>')
-                        var N = jsn.length;
-                        for (i = 0; N > i; i++) {
-                            console.log((0 == jsn[i].coursecount) ? ('–') : (jsn[i].coursecount));
-                            $('#tbl_data').append('<tr>'
-                                    + '<td>' + jsn[i].id + '</td>'
-                                    + '<td><a target="_blank" href="https://study.edu.tele-med.ai/course/index.php?categoryid=' + jsn[i].id + '">' + jsn[i].name + '</a>' + '</td>'
-                                    + '<td>' + ((0 == jsn[i].coursecount) ? ('–') : (jsn[i].coursecount)) + '</td>'
-                                    + '<td>' + getParent(jsn, jsn[i].parent) + '</td>'
-                                    + '</tr>');
-                        }
-                    }
-                }
-            }
-    );
+    };
+    var response = $.ajax({
+        type: 'GET',
+        data: data,
+        dataType: "json", // тип загружаемых данных
+        url: serverurl,
+        success: function (data, textStatus) {
+            //var jsn = data;
+            console.log(data);
+            getDataFromMoodle(data);
+        }
+    });
 });
